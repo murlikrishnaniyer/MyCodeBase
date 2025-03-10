cef4fa0195053f6b49cc30db02e604b6326bd38c
muralikrishnan.iyer <muralikrishnan.iyer@LTPBAN332364816.corp.mphasis.com>
2025-03-11 00:48:46 +0530

    Made changes to deploy to yml by configuring the ecr auth

--- src/main/java/com/example/springbootk8s/controller/SpringBootK8sController.java
+++ src/main/java/com/example/springbootk8s/controller/SpringBootK8sController.java
@@ -1,12 +1,12 @@
 package com.example.springbootk8s.controller;
 
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 
 public class SpringBootK8sController {
-	@RequestMapping("/")
+	@GetMapping("/")
 	public String index() {
 		return "Greetings from Spring Boot!";
 	}
